48f8f6fd45c52095afb8a37d7ab2290427651497
Anderson Santos <anderson.santos@lambda3.com.br>
2021-07-09 11:19:32 -0300

    feat: transfer and sync blocks

--- src/hospital/aula6.clj
+++ src/hospital/aula6.clj
@@ -2,8 +2,16 @@
   (:use [clojure.pprint])
   (:require [hospital.model :as h.model]))
 
-(defn chega-em [fila pessoa]
-  (conj fila pessoa))
+(defn cabe-na-fila? [fila]
+  (-> fila
+      count
+      (< 5)))
+
+(defn chega-em
+  [fila pessoa]
+  (if (cabe-na-fila? fila)
+    (conj fila pessoa)
+    (throw (ex-info "Fila já está cheia" {:tentando-adicionar pessoa}))))
 
 (defn chega-em!
   "Troca de referência via ref-set"
@@ -22,7 +30,34 @@
                   :laboratorio1 (ref h.model/fila-vazia)
                   :laboratorio2 (ref h.model/fila-vazia)
                   :laboratorio3 (ref h.model/fila-vazia)}]
-    (dosync (chega-em! hospital "Guilherme"))
+    (dosync
+      (chega-em! hospital "Guilherme")
+      (chega-em! hospital "Maria")
+      (chega-em! hospital "Lúcia")
+      (chega-em! hospital "Daniela")
+      (chega-em! hospital "Ana")
+      ;(chega-em! hospital "Paulo")
+      )
     (pprint hospital)))
 
-(simula-um-dia)
+; (simula-um-dia)
+
+(defn async-chega-em! [hospital pessoa]
+  (future
+    (Thread/sleep (rand 5000))
+    (dosync
+      (println "Tentando o código sincronizado" pessoa)
+      (chega-em! hospital pessoa))))
+
+(defn simula-um-dia-async []
+  (let [hospital {:espera (ref h.model/fila-vazia)
+                  :laboratorio1 (ref h.model/fila-vazia)
+                  :laboratorio2 (ref h.model/fila-vazia)
+                  :laboratorio3 (ref h.model/fila-vazia)}]
+    (dotimes [pessoa 10]
+      (async-chega-em! hospital pessoa))
+    (future
+      (Thread/sleep 8000)
+      (pprint hospital))))
+
+(simula-um-dia-async)
